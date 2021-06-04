ee25d88a6e01707f41b4f764e9a25c7433899680
Mathew <matymcleod@gmail.com>
2021-06-04 03:05:07 +0000

    completed first survey.js

---
+++ survey.js
@@ -0,0 +1,26 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let array = [];
+rl.question('What is your name? ', (answer) => {
+  array.push(`your name is ${answer}`);
+    rl.question('How old is ya? ', (answer) => {
+      array.push(`you are: ${answer}`);
+        l.question('Where do you live? ', (answer) => {
+          array.push(`You live in ${answer}`);
+          console.log(`These are you answers: ${array}`)
+        rl.close();
+    });
+  });
+});
+
+  
+
+
+
+
+
+
